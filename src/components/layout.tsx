82c95bf40df107b10f2093d382f2a959f0a277c2
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2021-10-26 13:02:41 +0500

    Layout setup

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,39 @@
 // import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+const Layout = ({ children }) => {
+  const authenticated = false;
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800 h-16 shadow-sm">
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="Home House"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={() => {}}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
